30780e5fec39f4dc81359207e435ff74eacf5066
Illya Yaroshevich <yaroshevich00@yndex.ru>
2021-07-31 18:24:10 +0300

    Создан общий маршрутный файл index.js

--- app.js
+++ app.js
@@ -2,16 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
-const { celebrate, Joi, errors } = require('celebrate');
-const NotFoundErr = require('./errors/not-found-err');
 const ServerErr = require('./middlewares/server-err');
 const { mongoLink } = require('./utils/constants');
-const usersRouter = require('./routes/users');
-const moviesRouter = require('./routes/movies');
-const { createUser, login } = require('./controllers/users');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const auth = require('./middlewares/auth');
 const cors = require('./middlewares/cors');
+const { errors } = require('celebrate');
+const router = require('./routes/index');
 
 require('dotenv').config();
 
@@ -32,36 +28,11 @@ mongoose.connect(NODE_ENV === 'production' ? CURR_URL : mongoLink, {
 app.use(cors);
 app.use(requestLogger);
 
-app.post('/signup',
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().required().min(2).max(20),
-      email: Joi.string().required().email(),
-      password: Joi.string().required().min(8),
-    }).unknown(true),
-  }),
-  createUser);
-
-app.post('/signin',
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required().min(8),
-    }),
-  }),
-  login);
-
-app.use('/users', auth, usersRouter);
-
-app.use('/movies', auth, moviesRouter);
+app.use('/', router)
 
 app.use(errorLogger);
 
 app.use(errors());
-
-app.use('/', () => {
-  throw new NotFoundErr('Запрашиваемый ресурс не найден');
-});
 
 app.use((err, req, res, next) => ServerErr(err, req, res, next));
 
---
+++ routes/index.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+const auth = require('../middlewares/auth');
+const usersRouter = require('./users');
+const moviesRouter = require('./movies');
+const { createUser, login } = require('../controllers/users');
+const { celebrate, Joi } = require('celebrate');
+const NotFoundErr = require('../errors/not-found-err');
+
+
+router.post('/signup',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(20),
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(8),
+    }).unknown(true),
+  }),
+  createUser);
+
+router.post('/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(8),
+    }),
+  }),
+  login);
+
+router.use('/users', auth, usersRouter);
+
+router.use('/movies', auth, moviesRouter);
+
+router.use('/', () => {
+  throw new NotFoundErr('Запрашиваемый ресурс не найден');
+});
+
+module.exports = router;
