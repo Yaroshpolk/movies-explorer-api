ddf6db982fe040310b13f07c816493d09e82f282
Yaroshpolk <yaroshevich.vlad00@mail.ru>
2021-09-05 13:55:40 +0300

    fixes

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
-const regUrl = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/;
+const regUrl = /https?:\/\/[\w{1,}\W{1,}]+#?\./;
 const mongoLink = 'mongodb://localhost:27017/bitfilmsdb';
 
 module.exports = {
